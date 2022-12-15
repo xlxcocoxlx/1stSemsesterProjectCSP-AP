bce95f5d36700edfd1c40d2f94019cc9f26e21b2
Cesar Ortiz <pollocesar2007@gmail.com>
2022-12-14 21:40:48 -0600

    first commit

---
+++ Class.py
@@ -0,0 +1,20 @@
+# so here i just make a python class or more recognize python "library"
+class firstOne:
+    def probably(self):
+        print("This is a python class")
+    def write():
+        print("This is a calling from a function inside of a class")
+    def no():
+        print("This is another calling from a function inside of a class")
+
+'''class secondOne:
+    def secondTesting(self):
+        print("I called this print from the Class.py file in the same folder")
+
+class thirdOne:
+    def __init__():
+        print("This is another way to call in the classes in a file")'''
+
+firstOne.probably()
+firstOne.write()
+firstOne.no()
---
+++ importing the class.py
@@ -0,0 +1,3 @@
+# I import the class from another file to this file and run it
+from Class import secondOne
+secondOne.secondTesting()
